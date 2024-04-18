c79608b01d20e647960abc36d968bdd75efe0896
CaptainNemo <rohitashwachaks@gmail.com>
2024-04-17 22:37:32 -0500

    Update portfolio.py

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,6 +1,7 @@
 import json
 from collections import defaultdict
 
+import numpy as np
 import pandas as pd
 import yfinance as yf
 from typing import Union, List, Dict
@@ -143,6 +144,18 @@ class PortfolioBaseClass:
     name: str
     assets: Dict[str, Asset] = field(default_factory=defaultdict(Asset))
     window: Union[None, int] = field(default=None)
+    weight: float = field(init=False)
+
+@dataclass
+class BasePortfolioClass:
+    def __init__(self, name: str,
+                 ) -> None:
+        self.name: str = name
+        self.asset: Asset = Asset()
+    name: str
+    assets: Asset = field(default_factory=Asset)
+    window: Union[None, int] = field(default=None)
+    weights: np.array = field(init=False)
 
 
 @dataclass
@@ -153,7 +166,7 @@ class Portfolio(PortfolioBaseClass):
                  nav: int = 100,
                  rebalance: int = 1,
                  reconstitute: int = 100,
-                 assets: Dict[str, Asset] = None,
+                 assets: Union[List[str], Dict[str, Asset], None] = None,
                  window: Union[None, int] = None,
                  trading_algo: Union[None, TradingAlgo] = None):
         """
@@ -166,15 +179,32 @@ class Portfolio(PortfolioBaseClass):
         :param rebalance: Number of months to rebalance the portfolio
         :param reconstitute: Number of months to reconstitute the portfolio
         """
+        self.IsWeightInitialised: bool = False
+
+        if isinstance(assets, List):
+            assets = {ticker: Asset(ticker) for ticker in assets}
+
         super().__init__(name=name,
                          assets=assets,
                          window=window)
-        self.rebalance: int = rebalance
-        self.reconstitute: int = reconstitute
-        self.trading_algo: TradingAlgo = trading_algo
-        self.nav: float = nav
 
-        self.IsWeightInitialised: bool = False
+        self.weights: Dict[str, float] = {x: 0 for x in self.assets.keys()}
+
+        self.start_nav = nav
+        self.prev_day_nav = nav
+        self.current_nav = nav
+
+        self.rebalancing_interval: int = rebalance
+        self.reconstitution_interval: int = reconstitute
+        self.last_rebalancing_date = datetime('1970-01-01').date()
+        self.last_reconstitution_date = datetime('1970-01-01').date()
+
+        # self.trading_algo: TradingAlgo = trading_algo
+        print("Building Portfolios and Initialising Trading Algorithms")
+        trade_module = __import__(f"trading_algo.{str.lower(trading_algo)}", fromlist=[trading_algo])
+        algo = getattr(trade_module, trading_algo)
+        self.trading_algo = algo()
+
         self.target: Asset = Asset(ticker=target,
                                    window=window,
                                    shares=1)
@@ -183,16 +213,25 @@ class Portfolio(PortfolioBaseClass):
     @property
     def tickerset(self):
         return self.assets.keys()
+
+    @property
+    def weights(self):
+
+
     # endregion Properties
+
     # region Functions
 
-    def update_tickerlist(self, date: pd.Timestamp) -> None:
-        self.tickers = self.ticker_set[date.year]
-        return
+    # def update_tickerlist(self, date: pd.Timestamp) -> None:
+    #     self.tickers = self.ticker_set[date.year]
+    #     return
 
     def isUpdateAllowed(self, date: pd.Timestamp, mode: str = "rebalance") -> bool:
         """
-            Calculate Number of months between two dates
+        Calculate Number of months between two dates and determine if it's time to update the weights
+        :param date:
+        :param mode:
+        :return:
         """
         diff = False
         if mode == "rebalance":
@@ -204,11 +243,16 @@ class Portfolio(PortfolioBaseClass):
             diff = diff >= self.reconstitution_interval
         return diff
 
+    # TODO
     def reconstitute(self, price: pd.DataFrame, date: pd.Timestamp, tickerlist: list) -> None:
         """
-            Function to Reconstitute our portfolio.
+        Function to Reconstitute our portfolio.
             Checks:
                 - weights updated only after "Reconstitution" period has passed
+        :param price:
+        :param date:
+        :param tickerlist:
+        :return:
         """
         print("Reconstitution on: " + date.strftime("%d-%b-%Y"))
 
@@ -232,91 +276,90 @@ class Portfolio(PortfolioBaseClass):
 
             self.rebalance(price=price, date=date)
         else:
-            raise Exception("Reconstitution Interval not passed.\n\t\
-                            Last Reconstituted on: {last_recons}\n\t\
-                            Requesting Reconstitution on: {curr_recons}".format(
+            raise RuntimeError("Reconstitution Interval not passed.\n\t" \
+                               "Last Reconstituted on: {last_recons}\n\t" \
+                               "Requesting Reconstitution on: {curr_recons}".format(
                 last_recons=self.last_reconstitution_date.strftime("%d-%b-%Y"),
                 curr_recons=date.strftime("%d-%b-%Y")))
         self.last_reconstitution_date = date
-        return
 
-    def rebalance(self, price, date: pd.Timestamp = None) -> None:
-        """
-            Function to update the Number of shares held by our portfolio.
-            Checks:
-                - Do ticker lists match?
-                - weights updated only after "rebalance" period has passed
-        """
-        print("\tRebalancing on: " + date.strftime("%d-%b-%Y"))
-
-        if self.isUpdateAllowed(date=date, mode="rebalance"):
-            difference_set = {x for x in price.index if x not in set(self.tickers)}
-            if len(difference_set) == 0:
-
-                if self.IsWeightInitialised:
-                    self.dividends = pd.Series([0.0] * len(self.tickers), index=self.tickers)
-                    self.prev_day_nav = self.current_nav
-                    self.current_nav = self.weights.multiply(price["close"]).sum() + self.dividends.sum()
-
-                self.weights = self.trading_algo.run(price, self.current_nav, date)
-                self.last_rebalancing_date = date
-                self.IsWeightInitialised = True
-            else:
-                raise Exception(" Input and Portfolio Tickers Not Matching. Net mismatch: {mismatch}\n".format(
-                    mismatch=len(difference_set)) + ", ".join(list(difference_set)));
-        else:
-            raise Exception("rebalance Interval not passed.\n\t\
-                            Last rebalanced on: {last_rebal}\n\t\
-                            Requesting rebalance on: {curr_rebal}".format(
-                last_rebal=self.last_rebalancing_date.strftime("%d-%b-%Y"),
-                curr_rebal=date.strftime("%d-%b-%Y")))
-        return
-
-    def run(self, price: pd.DataFrame, date: pd.Timestamp, tickerlist: list = None) -> tuple:
-        """
-            This function will be run on every date
-        """
-        price.fillna(0, inplace=True)
-        self.dividends += price["div"]
-
-        if sum(price.shares) != 0:
-            if self.isUpdateAllowed(date, mode="reconstitute"):
-                self.reconstitute(date=date, price=price, tickerlist=tickerlist)
-            else:
-                price = price[price.apply(lambda x: (x.name in self.tickers), axis=1)]
-
-                if price.shape[0] == 0:
-                    return -1, -1
-                if self.isUpdateAllowed(date):
-                    self.rebalance(date=date, price=price)
-                else:
-                    self.prev_day_nav = self.current_nav
-                    self.current_nav = self.weights.multiply(price["close"]).sum() + self.dividends.sum()
-
-        return self.current_nav, self.get_returns()
-
-    def get_nav(self, price: pd.DataFrame) -> float:
-        nav = self.weights.multiply(price["close"]).sum() + self.dividends.sum()
-        return nav
-
-    def get_returns(self) -> float:
-        """
-            Function to calculate returns of our portfolio
-        """
-        return (self.current_nav - self.prev_day_nav + self.dividends.sum()) / self.prev_day_nav
-
-    def echo(self) -> None:
-        print("Target Index:", self.target)
-        print("Trading Algorithm:", self.trading_algo.identifier)
-        print("Starting Investment:", self.start_nav)
-        print("Current Investment:", self.current_nav)
-        if len(self.tickers) > 0:
-            print("Constituent Indices:", ", ".join(self.tickers[0:5] + ["\b\b "]))
-        if len(self.weights) > 0:
-            print("Constituent Weights:", self.weights[0:5])
-        pass
-
-    # endregion Functions
+    # def rebalance(self, price, date: pd.Timestamp = None) -> None:
+    #     """
+    #         Function to update the Number of shares held by our portfolio.
+    #         Checks:
+    #             - Do ticker lists match?
+    #             - weights updated only after "rebalance" period has passed
+    #     """
+    #     print("\tRebalancing on: " + date.strftime("%d-%b-%Y"))
+    #
+    #     if self.isUpdateAllowed(date=date, mode="rebalance"):
+    #         difference_set = {x for x in price.index if x not in set(self.tickers)}
+    #         if len(difference_set) == 0:
+    #
+    #             if self.IsWeightInitialised:
+    #                 self.dividends = pd.Series([0.0] * len(self.tickers), index=self.tickers)
+    #                 self.prev_day_nav = self.current_nav
+    #                 self.current_nav = self.weights.multiply(price["close"]).sum() + self.dividends.sum()
+    #
+    #             self.weights = self.trading_algo.run(price, self.current_nav, date)
+    #             self.last_rebalancing_date = date
+    #             self.IsWeightInitialised = True
+    #         else:
+    #             raise Exception(" Input and Portfolio Tickers Not Matching. Net mismatch: {mismatch}\n".format(
+    #                 mismatch=len(difference_set)) + ", ".join(list(difference_set)));
+    #     else:
+    #         raise Exception("rebalance Interval not passed.\n\t\
+    #                         Last rebalanced on: {last_rebal}\n\t\
+    #                         Requesting rebalance on: {curr_rebal}".format(
+    #             last_rebal=self.last_rebalancing_date.strftime("%d-%b-%Y"),
+    #             curr_rebal=date.strftime("%d-%b-%Y")))
+    #     return
+    #
+    # def run(self, price: pd.DataFrame, date: pd.Timestamp, tickerlist: list = None) -> tuple:
+    #     """
+    #         This function will be run on every date
+    #     """
+    #     price.fillna(0, inplace=True)
+    #     self.dividends += price["div"]
+    #
+    #     if sum(price.shares) != 0:
+    #         if self.isUpdateAllowed(date, mode="reconstitute"):
+    #             self.reconstitute(date=date, price=price, tickerlist=tickerlist)
+    #         else:
+    #             price = price[price.apply(lambda x: (x.name in self.tickers), axis=1)]
+    #
+    #             if price.shape[0] == 0:
+    #                 return -1, -1
+    #             if self.isUpdateAllowed(date):
+    #                 self.rebalance(date=date, price=price)
+    #             else:
+    #                 self.prev_day_nav = self.current_nav
+    #                 self.current_nav = self.weights.multiply(price["close"]).sum() + self.dividends.sum()
+    #
+    #     return self.current_nav, self.get_returns()
+    #
+    # def get_nav(self, price: pd.DataFrame) -> float:
+    #     nav = self.weights.multiply(price["close"]).sum() + self.dividends.sum()
+    #     return nav
+    #
+    # def get_returns(self) -> float:
+    #     """
+    #         Function to calculate returns of our portfolio
+    #     """
+    #     return (self.current_nav - self.prev_day_nav + self.dividends.sum()) / self.prev_day_nav
+    #
+    # def echo(self) -> None:
+    #     print("Target Index:", self.target)
+    #     print("Trading Algorithm:", self.trading_algo.identifier)
+    #     print("Starting Investment:", self.start_nav)
+    #     print("Current Investment:", self.current_nav)
+    #     if len(self.tickers) > 0:
+    #         print("Constituent Indices:", ", ".join(self.tickers[0:5] + ["\b\b "]))
+    #     if len(self.weights) > 0:
+    #         print("Constituent Weights:", self.weights[0:5])
+    #     pass
+    #
+    # # endregion Functions
 
 
 if __name__ == '__main__':
@@ -334,11 +377,11 @@ if __name__ == '__main__':
     # endregion Assets
 
     portfolio = Portfolio(name='Stonks',
-                  target='^GSPY',
-                  assets={
-                      'AAPL': aapl,
-                      'MSFT': msft,
-                      'TSLA': tsla
-                  },
-                  nav=1000)
+                          target='^GSPY',
+                          assets={
+                              'AAPL': aapl,
+                              'MSFT': msft,
+                              'TSLA': tsla
+                          },
+                          nav=1000)
     p
